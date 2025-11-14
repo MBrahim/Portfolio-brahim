d89aa28bf18e3384419fcea003266ac68b3f062b
Mohamed Ahmed <89361937+MBrahim@users.noreply.github.com>
2025-11-14 17:29:44 +0200

    completed Home and added (under dev) in Projects

---
+++ src/components/Projects/16-Channel-Smart-Home-Control-Module.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Particle from "../Particle";
+import Techstack from "../About/Techstack";
+
+function SHM() {
+    return (
+        <Container fluid className="project-section">
+            <Particle />
+            <Container>
+                 <h1 className="project-heading">
+                 <strong className="purple">UNDER-DEV</strong>
+                </h1>
+            </Container>
+        </Container>
+)}
+
+export default SHM;
